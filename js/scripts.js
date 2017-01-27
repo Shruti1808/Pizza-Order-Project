77433a2caf7b5954f07090860d26687d6b67f76e
Shruti Priya <shrutipriya1808@gmail.com>
2017-01-27 13:42:51 -0800

    make changes in the code to get the input from the user and display the total cost

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var Pizza = function(pizzaSize,pizzaTopping){    //Constructor
 this.pizzaSize= pizzaSize;
 this.pizzaTopping = pizzaTopping;
-
+}
 Pizza.prototype.totalCost =function(){        //Prototype
   var costOfSmallPizza=10;
   var costOfMediumPizza =15;
@@ -11,8 +11,8 @@ Pizza.prototype.totalCost =function(){        //Prototype
   var costOfCheeseTopping= 2;
   var costOfArtichokeTopping =3;
   var costOfPepperoniTopping=5;
-  var cost = 0;
-  if(this.pizzaSize ==="small"){
+  var cost = 0 ;
+  if(this.pizzaSize ==="small"){           //get pizza cost based on their size
     cost = costOfSmallPizza;
   }else if (this.pizzaSize ==="medium"){
     cost = costOfMediumPizza;
@@ -21,30 +21,34 @@ Pizza.prototype.totalCost =function(){        //Prototype
   }else if (this.pizzaSize === "extra-large") {
     cost = costOfExtraLargePizza;
 }
-this.pizzaTopping.forEach(function(){
-   if(this.pizzaTopping ==="cheese"){
+
+   if(this.pizzaTopping ==="cheese"){     //get pizza cost based on their topping
      cost = cost + costOfCheeseTopping;
    }else if (this.pizzaTopping ==="artichoke") {
      cost = cost + costOfArtichokeTopping;
    }else if (this.pizzaTopping ==="pepperoni") {
      cost = cost + costOfPepperoniTopping;
    }
-  })
+
   return cost;
  }
-}
+
 
 // User-interface logic goes here
 $(document).ready(function(){
   $("form#pizza").submit(function(event){
     event.preventDefault();
 
-  //  Take Input from user to order pizza
-    var inputtedpizzaSize = $("input.size").val();
-    var inputtedpizzaTopping = $("input.topping").val();
 
-    var userTopping= ["cheese","pepperoni","artichoke"];
-    var userSize = ["small","medium","large","extra-large"];
+  //  Take Input from user to order pizza
+    var inputtedpizzaSize = $("input:radio[name=pizza-size]:checked").val();
+    var inputtedpizzaTopping = $("input:checkbox[name=topping]:checked").val();
+
+    // var userTopping= ["cheese","pepperoni","artichoke"];
+    // var userSize = ["small","medium","large","extra-large"];
     var myPizza = new Pizza(inputtedpizzaSize,inputtedpizzaTopping);  //Create object using constructor//
 
-    var totalCost = " Total Cost : $" + myPizza.totalCost() + ".00";
+    var userCost = (" Total Cost : $" + myPizza.totalCost() + ".00");
+    $(".totalCost").text(userCost);
+})
+})
